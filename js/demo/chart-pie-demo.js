53906260fce0ed3acb65733d8a048c28634ec3a9
Thiago Prado Marçal <thiago.marcal@edu.unifil.br>
2024-12-05 16:22:59 -0300

    Subindo projeto

---
+++ js/demo/chart-pie-demo.js
@@ -0,0 +1,36 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Direct", "Referral", "Social"],
+    datasets: [{
+      data: [55, 30, 15],
+      backgroundColor: ['#FFB74D', '#FF8A65', '#F06292'], // Cores principais (tons quentes)
+      hoverBackgroundColor: ['#FFA726', '#FF7043', '#E91E63'], // Cores no hover (mais escuras)
+      hoverBorderColor: "rgba(234, 236, 244, 1)", // Borda no hover
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
